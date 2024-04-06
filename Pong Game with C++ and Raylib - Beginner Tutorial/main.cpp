f7b64ea2b588b3e2b8dadd3fe19745c3d1aa5c97
khristinatsymbala <khristinakorda@gmail.com>
2024-04-06 22:05:51 +0300

    Draw the paddle and the ball

--- Pong Game with C++ and Raylib - Beginner Tutorial/main.cpp
+++ Pong Game with C++ and Raylib - Beginner Tutorial/main.cpp
@@ -18,6 +18,14 @@ int main(){
     {
         BeginDrawing();
 
+        //Drawing=================================
+        DrawLine(windowWidth/2,0,windowWidth/2,windowHeight,WHITE);
+
+        DrawCircle(windowWidth/2, windowHeight/2, 20, WHITE);
+
+
+        DrawRectangle(10, windowHeight/2 - 60, 30, 120, WHITE);
+        DrawRectangle(windowWidth - 35, windowHeight/2 - 60, 30, 120, WHITE);
 
 
         EndDrawing();
