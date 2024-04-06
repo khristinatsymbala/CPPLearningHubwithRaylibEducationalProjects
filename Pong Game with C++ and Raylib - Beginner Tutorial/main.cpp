c815d0c68fa4533d3eed852c42dc7f20760043b0
khristinatsymbala <khristinakorda@gmail.com>
2024-04-06 21:53:44 +0300

    Create a blanc screen & Game loop

--- Pong Game with C++ and Raylib - Beginner Tutorial/main.cpp
+++ Pong Game with C++ and Raylib - Beginner Tutorial/main.cpp
@@ -1,2 +1,29 @@
 #include <raylib.h>
+#include <iostream>
+
+int main(){
+
+
+//===Variables====================================
+    const int windowWidth{1280};
+    const int windowHeight{800};
+    
+//window ========================================
+    
+    InitWindow(windowWidth,windowHeight,"Pong Game!");
+
+//Game loop ======================================
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+
+
+        EndDrawing();
+    }
+    
+    CloseWindow();
+
+}
 
