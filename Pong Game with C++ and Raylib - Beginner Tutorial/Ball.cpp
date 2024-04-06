82a4077ac1779f964107ab58df8bebdd1ea9bced
khristinatsymbala <khristinakorda@gmail.com>
2024-04-06 22:39:48 +0300

    Check the collition

--- Pong Game with C++ and Raylib - Beginner Tutorial/Ball.cpp
+++ Pong Game with C++ and Raylib - Beginner Tutorial/Ball.cpp
@@ -9,6 +9,19 @@ void Ball::Update()
 {
     x += speedX;
     y += speedY;
+
+    //colition with screen
+    if (y + radius >= GetScreenHeight() || y - radius <= 0)
+    {
+        speedY *= -1;
+    }
+
+    if (x + radius >= GetScreenWidth() || x - radius <= 0)
+    {
+        speedX *= -1;
+    }
+    
+    
 }
 
 void Ball::SetterSpeedX(int sX)
