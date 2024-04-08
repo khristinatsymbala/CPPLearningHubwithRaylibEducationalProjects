7edd483ca37488b518d1d41cf53fc9891bf39773
khristinatsymbala <khristinakorda@gmail.com>
2024-04-08 16:22:32 +0300

    Setup a game loop

--- YouTube tutorials/tetris/main.cpp
+++ YouTube tutorials/tetris/main.cpp
@@ -3,12 +3,16 @@
 
 int main(){
 
+    Color darkBlue = {44, 44, 127, 255};
+
+
     InitWindow(300, 600, "Tetris Game");
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
         BeginDrawing();
+        ClearBackground(darkBlue);
 
         EndDrawing();
     }
