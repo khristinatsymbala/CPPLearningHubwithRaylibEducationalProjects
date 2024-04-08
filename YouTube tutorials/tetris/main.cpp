139af33b47091f5134ab39f52d8114e0eaaa004e
khristinatsymbala <khristinakorda@gmail.com>
2024-04-08 16:18:22 +0300

    setup a tetris game

---
+++ YouTube tutorials/tetris/main.cpp
@@ -0,0 +1,17 @@
+#include <raylib.h>
+#include <iostream>
+
+int main(){
+
+    InitWindow(300, 600, "Tetris Game");
+
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        EndDrawing();
+    }
+    
+
+}
